3269d4e5d1c0e77843ef940f4c485bfb1d7494ba
d <darinm223@gmail.com>
2021-10-18 13:49:00 -0700

    Fills out doStm and doExp

--- src/Tiger/Canon.hs
+++ src/Tiger/Canon.hs
@@ -25,6 +25,10 @@ instance Fn (a -> a -> a -> b) a b where
   fn f (a:b:c:_) = f a b c
   fn _ _ = error errStr
 
+instance Fn (a -> [a] -> b) a b where
+  fn f (a:rest) = f a rest
+  fn _ _ = error errStr
+
 linearize :: Supply -> Stm -> [Stm]
 linearize s0 = flip linear [] . doStm s0
  where
@@ -43,14 +47,28 @@ linearize s0 = flip linear [] . doStm s0
   reorderStm s l build = uncurry SeqStm $ build <$> reorder s l
   reorderExp s l build = build <$> reorder s l
 
+  doStm :: Supply -> Stm -> Stm
   doStm s (JumpStm e labs) = reorderStm s [e] (fn (`JumpStm` labs))
   doStm s (CJumpStm p a b t f) =
     reorderStm s [a, b] (fn (\a' b' -> CJumpStm p a' b' t f))
-  doStm _ _ = undefined
+  doStm s (ExpStm (CallExp f args)) =
+    reorderStm s (f:args) (fn ((ExpStm .) . CallExp))
+  doStm s (ExpStm e) = reorderStm s [e] (fn ExpStm)
+  doStm s (MoveStm (TempExp t) (CallExp f args)) =
+    reorderStm s (f:args) (fn ((MoveStm (TempExp t) .) . CallExp))
+  doStm s (MoveStm (TempExp t) b) = reorderStm s [b] (fn (MoveStm (TempExp t)))
+  doStm s (MoveStm (MemExp e) b) = reorderStm s [e, b] (fn (MoveStm . MemExp))
+  doStm s (MoveStm (ESeqExp stm e) b) = doStm s (SeqStm stm (MoveStm e b))
+  doStm (S _ l r) (SeqStm a b) = SeqStm (doStm l a) (doStm r b)
+  doStm _ stm = stm
 
+  doExp :: Supply -> Exp -> (Stm, Exp)
   doExp s (BinOpExp p a b) = reorderExp s [a, b] (fn (BinOpExp p))
   doExp s (MemExp a) = reorderExp s [a] (fn MemExp)
-  doExp _ _ = undefined
+  doExp s (CallExp f args) = reorderExp s (f:args) (fn CallExp)
+  doExp (S _ l r) (ESeqExp s e) = (SeqStm (doStm l s) stm', e')
+   where (stm', e') = doExp r e
+  doExp s e = reorderExp s [] (const e)
 
   linear (SeqStm a b) l = linear a (linear b l)
   linear s l = s:l
