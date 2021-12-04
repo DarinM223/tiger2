f69cc966f15686a28f3a2d5886d513290204280a
D M <darinm223@gmail.com>
2021-12-04 12:26:26 -0800

    Adds MipsGen monad

--- src/Tiger/MipsGen.hs
+++ src/Tiger/MipsGen.hs
@@ -1,11 +1,22 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
 module Tiger.MipsGen where
 
-import Control.Applicative (liftA3)
+import Control.Applicative (liftA2, liftA3)
+import Control.Monad.Reader (MonadIO, MonadReader, ReaderT (ReaderT))
+import Data.IORef
 import Tiger.Codegen
-import Tiger.Temp (MonadTemp (newTemp), Temp)
+import Tiger.MipsFrame (MipsFrame)
+import Tiger.Symbol (Gen, MonadSymbol)
+import Tiger.Temp
 import Tiger.Translate (MonadPut (put))
 import Tiger.Tree
+
+calldefs :: [Temp]
+calldefs = undefined
 
 munchStm :: (MonadTemp m, MonadPut Instr m) => Stm -> m ()
 munchStm (SeqStm a b) = munchStm a >> munchStm b
@@ -24,8 +35,14 @@ munchStm (MoveStm (MemExp e1) e2) = put =<< liftA3
 munchStm (MoveStm (TempExp i) e2) = put =<< liftA3
   (OperInstr "move `d0, `s0")
   (traverse munchExp [e2]) (pure [i]) (pure Nothing)
+munchStm (ExpStm (CallExp e args)) = put =<< liftA3
+  (OperInstr "jalr `s0")
+  (liftA2 (:) (munchExp e) (munchArgs 0 args)) (pure calldefs) (pure Nothing)
 munchStm (LabelStm lab) = put $ OperInstr (show lab ++ ":") [] [] Nothing
 munchStm s = error $ "munchStm: unknown statement " ++ show s
+
+munchArgs :: Int -> [Exp] -> m [Temp]
+munchArgs = undefined
 
 munchExp :: (MonadTemp m, MonadPut Instr m) => Exp -> m Temp
 munchExp (MemExp (BinOpExp Plus e1 (ConstExp i))) = result $ \r ->
@@ -55,3 +72,25 @@ munchExp e = error $ "munchExp: unknown expression " ++ show e
 
 result :: MonadTemp m => (Temp -> m a) -> m Temp
 result f = newTemp >>= \t -> t <$ f t
+
+data GenState = GenState
+  { _symbolGen :: Gen
+  , _tempGen   :: IO Temp
+  , instrList  :: IORef [Instr]
+  }
+
+newtype MipsGen a = MipsGen (GenState -> IO a)
+  deriving (Functor, Applicative, Monad, MonadIO, MonadReader GenState)
+    via ReaderT GenState IO
+  deriving MonadSymbol via SymbolFromField "_symbolGen" MipsGen
+  deriving MonadTemp via TempFromField "_tempGen" MipsGen
+
+instance MonadPut Instr MipsGen where
+  put f = MipsGen $ flip modifyIORef' (f :) . instrList
+
+instance MonadCodegen MipsGen where
+  type Frame' MipsGen = MipsFrame
+  codegen _ s = munchStm s >> MipsGen (fmap reverse . readIORef . instrList)
+
+runMipsGen :: Gen -> IO Temp -> MipsGen a -> IO a
+runMipsGen sym tmp (MipsGen f) = newIORef [] >>= f . GenState sym tmp
