169e8542d465602aebbd7a6fb280a0b55c183d4d
D M <darinm223@gmail.com>
2021-12-11 13:48:29 -0800

    Implements munchExp binary operators

--- src/Tiger/MipsGen.hs
+++ src/Tiger/MipsGen.hs
@@ -30,9 +30,8 @@ munchStm (SeqStm a b) = munchStm a >> munchStm b
 munchStm (MoveStm (MemExp (BinOpExp Plus e1 (ConstExp i))) e2) = emit =<< liftA3
   (OperInstr ("sw `s1, " ++ show i ++ "(`s0)"))
   (traverse munchExp [e1, e2]) (pure []) (pure Nothing)
-munchStm (MoveStm (MemExp (BinOpExp Plus (ConstExp i) e1)) e2) = emit =<< liftA3
-  (OperInstr ("sw `s1, " ++ show i ++ "(`s0)"))
-  (traverse munchExp [e1, e2]) (pure []) (pure Nothing)
+munchStm (MoveStm (MemExp (BinOpExp Plus (ConstExp i) e1)) e2) =
+  munchStm (MoveStm (MemExp (BinOpExp Plus e1 (ConstExp i))) e2)
 munchStm (MoveStm (MemExp (ConstExp i)) e2) = emit =<< liftA3
   (OperInstr ("sw `s0, " ++ show i ++ "($zero)"))
   (traverse munchExp [e2]) (pure []) (pure Nothing)
@@ -57,27 +56,89 @@ munchExp :: (MonadTemp m, MonadPut Instr m, F.Frame frame)
 munchExp (MemExp (BinOpExp Plus e1 (ConstExp i))) = result $ \r ->
   emit =<< liftA3 (OperInstr ("lw `d0, " ++ show i ++ "(`s0)"))
     (traverse munchExp [e1]) (pure [r]) (pure Nothing)
-munchExp (MemExp (BinOpExp Plus (ConstExp i) e1)) = result $ \r ->
-  emit =<< liftA3 (OperInstr ("lw `d0, " ++ show i ++ "(`s0)"))
-    (traverse munchExp [e1]) (pure [r]) (pure Nothing)
+munchExp (MemExp (BinOpExp Plus (ConstExp i) e1)) =
+  munchExp (MemExp (BinOpExp Plus e1 (ConstExp i)))
 munchExp (MemExp (ConstExp i)) = result $ \r ->
   emit $ OperInstr ("lw `d0, " ++ show i ++ "($zero)") [] [r] Nothing
 munchExp (MemExp e) = result $ \r ->
   emit =<< liftA3 (OperInstr "lw `d0, 0(`s0)")
     (traverse munchExp [e]) (pure [r]) (pure Nothing)
+
 munchExp (BinOpExp Plus e1 (ConstExp i)) = result $ \r ->
   emit =<< liftA3 (OperInstr ("addi `d0, `s0, " ++ show i))
     (traverse munchExp [e1]) (pure [r]) (pure Nothing)
-munchExp (BinOpExp Plus (ConstExp i) e1) = result $ \r ->
-  emit =<< liftA3 (OperInstr ("addi `d0, `s0, " ++ show i))
-    (traverse munchExp [e1]) (pure [r]) (pure Nothing)
+munchExp (BinOpExp Plus (ConstExp i) e1) =
+  munchExp (BinOpExp Plus e1 (ConstExp i))
 munchExp (BinOpExp Plus e1 e2) = result $ \r ->
   emit =<< liftA3 (OperInstr "add `d0, `s0, `s1")
     (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+
+munchExp (BinOpExp Minus e1 (ConstExp i)) =
+  munchExp (BinOpExp Plus e1 (ConstExp (-i)))
+munchExp (BinOpExp Minus e1 e2) = result $ \r ->
+  emit =<< liftA3 (OperInstr "sub `d0, `s0, `s1")
+    (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+
+munchExp (BinOpExp Mul e1 e2) = result $ \r ->
+  emit =<< liftA3 (OperInstr "mult `d0, `s0, `s1")
+    (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+munchExp (BinOpExp Div e1 e2) = result $ \r ->
+  emit =<< liftA3 (OperInstr "div `d0, `s0, `s1")
+    (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+
+munchExp (BinOpExp And e1 (ConstExp i)) = result $ \r ->
+  emit =<< liftA3 (OperInstr ("andi `d0, `s0, " ++ show i))
+    (traverse munchExp [e1]) (pure [r]) (pure Nothing)
+munchExp (BinOpExp And (ConstExp i) e1) =
+  munchExp (BinOpExp And e1 (ConstExp i))
+munchExp (BinOpExp And e1 e2) = result $ \r ->
+  emit =<< liftA3 (OperInstr "and `d0, `s0, `s1")
+    (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+
+munchExp (BinOpExp Or e1 (ConstExp i)) = result $ \r ->
+  emit =<< liftA3 (OperInstr ("ori `d0, `s0, " ++ show i))
+    (traverse munchExp [e1]) (pure [r]) (pure Nothing)
+munchExp (BinOpExp Or (ConstExp i) e1) = munchExp (BinOpExp Or e1 (ConstExp i))
+munchExp (BinOpExp Or e1 e2) = result $ \r ->
+  emit =<< liftA3 (OperInstr "or `d0, `s0, `s1")
+    (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+
+munchExp (BinOpExp Xor e1 (ConstExp i)) = result $ \r ->
+  emit =<< liftA3 (OperInstr ("xori `d0, `s0, " ++ show i))
+    (traverse munchExp [e1]) (pure [r]) (pure Nothing)
+munchExp (BinOpExp Xor (ConstExp i) e1) =
+  munchExp (BinOpExp Xor e1 (ConstExp i))
+munchExp (BinOpExp Xor e1 e2) = result $ \r ->
+  emit =<< liftA3 (OperInstr "xor `d0, `s0, `s1")
+    (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+
+munchExp (BinOpExp Lshift e1 (ConstExp i)) = result $ \r ->
+  emit =<< liftA3 (OperInstr ("sll `d0, `s0, " ++ show i))
+    (traverse munchExp [e1]) (pure [r]) (pure Nothing)
+munchExp (BinOpExp Lshift e1 e2) = result $ \r ->
+  emit =<< liftA3 (OperInstr "sllv `d0, `s0, `s1")
+    (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+
+munchExp (BinOpExp Rshift e1 (ConstExp i)) = result $ \r ->
+  emit =<< liftA3 (OperInstr ("srl `d0, `s0, " ++ show i))
+    (traverse munchExp [e1]) (pure [r]) (pure Nothing)
+munchExp (BinOpExp Rshift e1 e2) = result $ \r ->
+  emit =<< liftA3 (OperInstr "srlv `d0, `s0, `s1")
+    (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+
+munchExp (BinOpExp Arshift e1 (ConstExp i)) = result $ \r ->
+  emit =<< liftA3 (OperInstr ("sra `d0, `s0, " ++ show i))
+    (traverse munchExp [e1]) (pure [r]) (pure Nothing)
+munchExp (BinOpExp Arshift e1 e2) = result $ \r ->
+  emit =<< liftA3 (OperInstr "srav `d0, `s0, `s1")
+    (traverse munchExp [e1, e2]) (pure [r]) (pure Nothing)
+
+munchExp (CallExp e args) = undefined
 munchExp (ConstExp i) = result $ \r ->
   emit $ OperInstr ("li `d0, " ++ show i) [] [r] Nothing
 munchExp (TempExp t) = pure t
-munchExp e = error $ "munchExp: unknown expression " ++ show e
+munchExp (NameExp lab) = undefined
+munchExp (ESeqExp s e) = munchStm s >> munchExp e
 
 data GenState = GenState
   { _symGen   :: SymGen
